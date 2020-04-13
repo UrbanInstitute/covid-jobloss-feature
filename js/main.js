4f36ecd7610d776a8ba4d81b78aa83939358c9df
Chartoff <bchartof@Informations-MacBook-Pro-2.local>
2020-04-13 06:57:52 -0400

    zoom and county highlight behavior

--- js/main.js
+++ js/main.js
@@ -1,59 +1,60 @@
 var dispatch = d3.dispatch("zoomOut", "zoomIn", "changeIndustry", "viewByCounty", "viewByCbsa");
 
 var cbsaAverageData,
-	countyAverageData,
-	countyBoundingBoxData,
-	cbsaBoundingBoxData,
-	cbsaToCounty,
-	countyToCbsa
+usAverageData,
+countyAverageData,
+countyBoundingBoxData,
+cbsaBoundingBoxData,
+cbsaToCounty,
+countyToCbsa
 
 var INIT_GEOID = "99"
 
 var industries = {
-"X01":"Agriculture, Forestry, Fishing, and Hunting",
-"X02":"Mining, Quarrying, and Oil and Gas Extractions",
-"X03":"Utilities",
-"X04":"Construction",
-"X05":"Manufacturing",
-"X06":"Wholesale Trade",
-"X07":"Retail Trade",
-"X08":"Transportation and Warehousing",
-"X09":"Information",
-"X10":"Finance and Insurance",
-"X11":"Real Estate and Rental and Leasing",
-"X12":"Professional, Scientific, and Technical Services",
-"X13":"Management of Companies and Enterprises",
-"X14":"Administrative and Support and Waste Management and Remediation Services",
-"X15":"Educational Services",
-"X16":"Health Care and Social Assistance",
-"X17":"Arts, Entertainment, and Recreation",
-"X18":"Accomodation and Food Services",
-"X19":"Other Services",
-"X20":"Public Administration",
+	"X01":"Agriculture, Forestry, Fishing, and Hunting",
+	"X02":"Mining, Quarrying, and Oil and Gas Extractions",
+	"X03":"Utilities",
+	"X04":"Construction",
+	"X05":"Manufacturing",
+	"X06":"Wholesale Trade",
+	"X07":"Retail Trade",
+	"X08":"Transportation and Warehousing",
+	"X09":"Information",
+	"X10":"Finance and Insurance",
+	"X11":"Real Estate and Rental and Leasing",
+	"X12":"Professional, Scientific, and Technical Services",
+	"X13":"Management of Companies and Enterprises",
+	"X14":"Administrative and Support and Waste Management and Remediation Services",
+	"X15":"Educational Services",
+	"X16":"Health Care and Social Assistance",
+	"X17":"Arts, Entertainment, and Recreation",
+	"X18":"Accomodation and Food Services",
+	"X19":"Other Services",
+	"X20":"Public Administration",
 }
 
 function wrap(text, width) {
-  text.each(function() {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        y = text.attr("y"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-    while (word = words.pop()) {
-      line.push(word);
-      tspan.text(line.join(" "));
-      if (tspan.node().getComputedTextLength() > width) {
-        line.pop();
-        tspan.text(line.join(" "));
-        line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-      }
-    }
-  });
+	text.each(function() {
+		var text = d3.select(this),
+		words = text.text().split(/\s+/).reverse(),
+		word,
+		line = [],
+		lineNumber = 0,
+		lineHeight = 1.1, // ems
+		y = text.attr("y"),
+		dy = parseFloat(text.attr("dy")),
+		tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+			while (word = words.pop()) {
+			line.push(word);
+			tspan.text(line.join(" "));
+			if (tspan.node().getComputedTextLength() > width) {
+				line.pop();
+				tspan.text(line.join(" "));
+				line = [word];
+				tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+			}
+		}
+	});
 }
 
 
@@ -63,7 +64,7 @@ function getBarW(){
 	return 420;
 }
 function getBarH(){
-	return 850;
+	return 550;
 }
 function getDotRadius(){
 	return 5;
@@ -96,27 +97,27 @@ function getClickedTractData(){
 
 
 function zoomOut(){
-	// change zoom icon
+	d3.select("#zoomOutIcon").transition().style("opacity",0)
 
-	setActiveBaseline("us", "us", true)
-	setActiveTract([], true)
+	// setActiveBaseline("us", "us", true)
+	// setActiveTract([], true)
 
 	dispatch.call("zoomOut")
 }
 
-function zoomIn(baselineType, geoid){
-	// change zoom icon
-	setActiveBaseline(baselineType, geoid, true)
-	setActiveTract([], true)
+function zoomIn(baselineType, geoid, coordinates){
+	d3.select("#zoomOutIcon").transition().style("opacity",1)
+	// setActiveBaseline(baselineType, geoid, true)
+	// setActiveTract([], true)
 
-	dispatch.call("zoomIn")
+	dispatch.call("zoomIn", null, coordinates)
 }
 
 function changeIndustry(industry){
 	//some kind of indication/highlight on bar chart. No value change of bars
 	// console.log(industry)
-	d3.selectAll(".barBg").style("fill", "#fff")
-	d3.select(".barBg." + industry.replace("cns","X")).style("fill", "#d2d2d2")
+	d3.selectAll(".barBg").style("fill", "transparent")
+	d3.select(".barBg." + industry).style("fill", "#d2d2d2")
 	dispatch.call("changeIndustry", null, industry)
 }
 function disableBaselineType(baselineType){
@@ -207,294 +208,372 @@ function changeBaselineType(newBaselineType){
 
 
 function initBarChart(countyAverageData){
-	// console.log(countyAverageData)
+	console.log(countyAverageData)
 	var svg = d3.select("#barChartContainer").append("svg")
 
-  var w = getBarW(),
-      h = getBarH(),
-      margin = getBarMargin(),
-      width = w - margin.left - margin.right,
-      height = h - margin.top - margin.bottom,
-      g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-      keys = ["baseline", "tract"]
+	var w = getBarW(),
+	h = getBarH(),
+	margin = getBarMargin(),
+	width = w - margin.left - margin.right,
+	height = h - margin.top - margin.bottom,
+	g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
+	keys = ["baseline", "tract"]
 
 
-    svg.attr("width", w).attr("height", h);
+	svg.attr("width", w).attr("height", h);
 
-var data = countyAverageData[INIT_GEOID]["vs"].sort(function(a,b){  return b.v - a.v })
-var y0 = d3.scaleBand()
-    .rangeRound([0, h - margin.top - margin.bottom])
-    .paddingInner(0.1)
-    .domain(Object.values(data.map(function(o){ return o.k })));
+	var data = countyAverageData[INIT_GEOID]["vs"].sort(function(a,b){  return b.v - a.v })
+	var y0 = d3.scaleBand()
+	.rangeRound([0, h - margin.top - margin.bottom])
+	.paddingInner(0.1)
+	.domain(Object.values(data.map(function(o){ return o.k })));
 
-var y1 = d3.scaleBand()
-    .padding(.2)
-    .domain(keys)
-    .rangeRound([15, y0.bandwidth()]);
+	var y1 = d3.scaleBand()
+	.padding(.2)
+	.domain(keys)
+	.rangeRound([15, y0.bandwidth()]);
 
-// console.log(d3.max(countyAverageData[INIT_GEOID]["vs"], function(o){ return o.v }))
-var x = d3.scaleLinear()
-.rangeRound([0,width-margin.right])
-  .domain([0,
-    // d3.max(countyAverageData[INIT_GEOID]["vs"], function(o){ return o.v })
-    75
-  ]);
-
-// console.log(data)
-
-  var gs = g
-    .selectAll("g.barGroup")
-    .data(data)
-    .enter().append("g")
-      .attr("class","barGroup")
-      .attr("transform", function(d) { return "translate(0," + y0(d.k) + ")"; })
-
-    gs.on("mouseover", function(d){
-    	changeIndustry(d.k)
-
-    })
+	var x = d3.scaleLinear()
+	.rangeRound([0,width-margin.right])
+	.domain([0,
+	// d3.max(countyAverageData[INIT_GEOID]["vs"], function(o){ return o.v })
+	100
+	]);
 
 
-    var bg = gs.append("rect")
-    	.attr("width", width)
-    	.attr("height", y0.bandwidth() + 10)
-    	.attr("class", function(d){ return "barBg " + d.k})
-    	.style("fill", "#fff")
+	var gs = g
+	.selectAll("g.barGroup")
+	.data(data)
+	.enter().append("g")
+	.attr("class",function(d){ return "barGroup " + d.k })
+	.attr("transform", function(d) { return "translate(0," + y0(d.k) + ")"; })
 
-    var baselineStick = gs
-      .append("line")
-      .attr("class", "baseline chartEl stick")
-      .attr("x1",x(0))
-      .attr("x2", function(d){ return x(d.v) })
-      .attr("y1", y1("baseline"))
-      .attr("y2", y1("baseline"))
+	gs.on("mouseover", function(d){
+	changeIndustry(d.k)
 
-    var baselineDot = gs
-      .append("circle")
-      .attr("class", "baseline chartEl dot")
-      .attr("cx", function(d){ return x(d.v) })
-      .attr("cy", y1("baseline"))
-      .attr("r", getDotRadius())
+	})
 
 
-    var tractStick = gs
-      .append("line")
-      .attr("class", "tract stick")
-      .attr("x1",x(0))
-      .attr("x2", x(0) )
-      .attr("y1", y1("tract"))
-      .attr("y2", y1("tract"))
+	var bg = gs.append("rect")
+	.attr("width", width)
+	.attr("height", y0.bandwidth() )
+	.attr("class", function(d){ return "barBg " + d.k})
+	.style("fill", "transparent")
 
-    var tractDot = gs
-      .append("circle")
-      .attr("class", "tract chartEl dot")
-      .attr("cx", x(0) )
-      .attr("cy", y1("tract"))
-      .attr("r", getDotRadius())
-      .style("opacity",0)
+	var baselineStick = gs
+	.append("line")
+	.attr("class", function(d){ return "baseline chartEl stick " + d.k })
+	.attr("x1",x(0))
+	.attr("x2", function(d){ return x(d.v) })
+	.attr("y1", y1("baseline"))
+	.attr("y2", y1("baseline"))
+
+	var baselineDot = gs
+	.append("circle")
+	.attr("class", function(d){ return "baseline chartEl dot " + d.k })
+	.attr("cx", function(d){ return x(d.v) })
+	.attr("cy", y1("baseline"))
+	.attr("r", getDotRadius())
 
 
-     var industryLabel = gs
-     	.append("text")
-     	.attr("class","industryLabel")
-     	.attr("y",10)
-     	.attr("dy",0)
-     	.text(function(d){ return industries[d.k] })
-      // .call(wrap, 230);
+	var tractStick = gs
+	.append("line")
+	.attr("class",function(d){ return "tract chartEl stick " + d.k })
+	.attr("x1",x(0))
+	// .attr("x2", x(0) )
+	.attr("x2", function(d){ return x(d.v)*.4 })
+	.attr("y1", y1("tract"))
+	.attr("y2", y1("tract"))
 
-     	// .attr("x", )
+	var tractDot = gs
+	.append("circle")
+	.attr("class",function(d){ return "tract chartEl dot " + d.k })
+	// .attr("cx", x(0) )
+	.attr("cx", function(d){ return x(d.v)*.4 })
+	.attr("cy", y1("tract"))
+	.attr("r", getDotRadius())
+	// .style("opacity",0)
+
+
+	var industryLabel = gs
+	.append("text")
+	.attr("class",function(d){ return "industryLabel " + d.k })
+	.attr("y",10)
+	.attr("dy",0)
+	.text(function(d){ return industries[d.k] })
+	// .call(wrap, 230);
+
 }
 
 
 function testUpdateBar(data){
 
-  var w = getBarW(),
-      h = getBarH(),
-      margin = getBarMargin(),
-      width = w - margin.left - margin.right
+var w = getBarW(),
+h = getBarH(),
+margin = getBarMargin(),
+width = w - margin.left - margin.right
 
-	var barData = []
-	for(key in data){
-		// console.log(key, data[key])
-		if(key.search("cns") != -1) barData.push({"k": key.replace("CNS","X"), "v": data[key]})
-	}
-// console.log(d3.max(barData, function(o){ return o.v }))
+var barData = []
+for(key in data){
+if(key.search("X") != -1 && key != "X000")barData.push({"k": key, "v": data[key]})
+}
 var x = d3.scaleLinear()
 .rangeRound([0,width-margin.right])
-  .domain([0,
-    // d3.max(barData, function(o){ return o.v })
-    75
-  ]);
+.domain([0,
+// d3.max(barData, function(o){ return o.v })
+100
+]);
 
 
-// barData = barData.sort(function(a,b){  return b.v - a.v })
+barData = barData.sort(function(a,b){  return b.v - a.v })
 var y0 = d3.scaleBand()
-    .rangeRound([0, h - margin.top - margin.bottom])
-    .paddingInner(0.1)
-    .domain(Object.values(barData.map(function(o){ return o.k })));
+.rangeRound([0, h - margin.top - margin.bottom])
+.paddingInner(0.1)
+.domain(Object.values(barData.map(function(o){ return o.k })));
 
 
 
+barData.forEach(function(b){
+// console.log(b)
+d3.select(".barGroup." + b.k)
+.transition()
+.attr("transform", function(d) { return "translate(0," + y0(b.k) + ")"; })
+
+d3.selectAll(".baseline.stick." + b.k)
+.transition()
+.attr("x2", function(d){ return x(b.v) })
+
+d3.select(".baseline.dot." + b.k)
+.transition()
+.attr("cx", function(d){ return x(b.v) })
+
+})
 
 
-// d3.selectAll(".barGroup")
-//     .data(barData)
-//     .transition()
-//       .attr("transform", function(d) { console.log(y0(d.k)); return "translate(0," + y0(d.k) + ")"; })
-
-
-d3.selectAll(".baseline.stick")
-.data(barData)
-      .transition()
-      .attr("x2", function(d){ if(d.k == "X18"){console.log(d.v)}; return x(d.v) })
-      // .attr("y1", y1("baseline"))
-      // .attr("y2", y1("baseline"))
-
-d3.selectAll(".baseline.dot")
-.data(barData)
-      .transition()
-      .attr("cx", function(d){ return x(d.v) })
-
-
-d3.selectAll(".industryLabel")
-.data(barData)
-     	// .attr("y",10)
-     	// .attr("dy",0)
-     	.text(function(d){ console.log(d.k); return industries[d.k.replace("cns","X")] })
-      // .call(wrap, 230);
-
-
-	 // {k: "X18", v: 43.8}
-// .selectAll("g.barGroup")
 }
 
 
 
 function initMap(){
-	mapboxgl.accessToken = 'pk.eyJ1IjoidXJiYW5pbnN0aXR1dGUiLCJhIjoiTEJUbmNDcyJ9.mbuZTy4hI_PWXw3C3UFbDQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidXJiYW5pbnN0aXR1dGUiLCJhIjoiTEJUbmNDcyJ9.mbuZTy4hI_PWXw3C3UFbDQ';
 
-	var map = new mapboxgl.Map({
-	attributionControl: false,
-	container: 'mapContainer',
-	style: 'mapbox://styles/urbaninstitute/ck8t0hd26024r1ipa18s35flo/draft',
-	center: [-95.5795, 39.8283],
-	zoom: 3
-	});
+var map = new mapboxgl.Map({
+attributionControl: false,
+container: 'mapContainer',
+style: 'mapbox://styles/urbaninstitute/ck8t0hd26024r1ipa18s35flo',
+center: [-95.5795, 39.8283],
+zoom: 3
+});
 
-	map.addControl(new mapboxgl.AttributionControl({
-        compact: true
-    }));
+map.addControl(new mapboxgl.AttributionControl({
+compact: true
+}));
 
-    map.scrollZoom.disable();
-
-	 
-	map.on('load', function() {
+map.scrollZoom.disable();
 
 
-
-
-		map.on('mousemove', 'county_test', function(e) {
-			// console.log(e.features[0].properties)
-			testUpdateBar(e.features[0].properties)
-		})
-
-
-
-		// click on county or cbsa
-			// 	zoomIn(baselineType, geoid)	
-
-		//on mouseover cbsa/county
-			// setActiveBaseline(cbsa/county, geoid, false)
-
-		//on mouseout cbsa/county
-			// setActiveBaseline(getClickedBaselineType(), getClickedBaseline(), true)
-
-
-
-		// click on tract
-			// 	setActiveTract(tractData, true)
-
-		//on mouseover tract
-			// setActiveTract(tractData, false)
-
-		//on mouseout tract
-			// setActiveTract(getClickedTractData(), true)
-
-
-
-
-
-		dispatch.on("zoomOut", function(){
-			// zoom da map out
-		})
-		dispatch.on("zoomIn", function(geoid){
-			//pan to bounding box
-
-			//select county or cbsa
-
-		})
-		dispatch.on("viewByCounty", function(){
-			//toggle layer
-			
-		})
-		dispatch.on("viewByCbsa", function(){
-			//toggle layer
-
-		})
-		dispatch.on("changeIndustry", function(industry){
-			//toggle layer
-
-
-			map.setPaintProperty("county_test", 'fill-color', [
-  "interpolate",
-  ["linear"],
-  ["get", industry.replace("X","cns")],
-  1,
-  "#cfe8f3",
-  2,
-  "#a2d4ec",
-  3,
-  "#73bfe2",
-  4,
-  "#46abdb",
-  5,
-  "#1696d2",
-  6,
-  "#12719e",
-  7,
-  "#0a4c6a",
-  8,
-  "#062635"
-]);
-			// console.log(industry)
-		})
-	})
+map.on('load', function() {
+var hideHoverData = {
+'type': 'Feature',
+'geometry': {
+'type': 'Polygon',
+'coordinates': [[]]
 }
+}
+map.addSource('hoverPolygonSource', {
+'type': 'geojson',
+'data': hideHoverData
+});
+map.addLayer({
+'id': 'hoverPolygon',
+'type': 'line',
+'source': 'hoverPolygonSource',
+'layout': {},
+'paint': {
+// 'fill-color': '#088',
+'line-color': "#fdbf11",
+'line-width': 3
+// 'fill-opacity': 0.8
+}
+});
+
+
+
+
+// map.on('mouseleave', 'county-fill', function(e) {
+// 	console.log(e)
+// })
+//on mouseover cbsa/county
+// setActiveBaseline(cbsa/county, geoid, false)
+map.on('mousemove', 'county-fill', function(e) {
+if(map.getZoom() > 3) return false
+testUpdateBar(e.features[0].properties)
+var data = {'type': 'Feature', 'geometry': e.features[0].geometry}
+map.getSource('hoverPolygonSource').setData(data);
+})
+
+map.on("click", "county-fill", function(e){
+
+var coordinates = e.features[0].geometry.coordinates[0]			
+zoomIn("county", e.features[0].properties.county_fips, coordinates)
+})
+
+
+
+
+// click on tract
+// 	setActiveTract(tractData, true)
+
+//on mouseover tract
+// setActiveTract(tractData, false)
+
+//on mouseout map
+map.on("mouseout", function(e){
+// console.log("rootabagearsa")
+setActiveTract(getClickedTractData(), true)
+})
+
+
+
+
+
+
+dispatch.on("zoomOut", function(){
+// zoom da map out
+map.getSource('hoverPolygonSource').setData(hideHoverData);
+map.setCenter([-95.5795, 39.8283])
+map.zoomTo(3)
+})
+dispatch.on("zoomIn", function(coordinates){
+//pan to bounding box
+var bounds = new mapboxgl.LngLatBounds()
+for(var i = 0; i < coordinates.length; i++){
+// console.log(coordinates[i])
+bounds.extend(coordinates[i])
+}
+
+map.fitBounds(bounds, {
+padding: 50
+});
+//select county or cbsa
+
+})
+dispatch.on("viewByCounty", function(){
+//toggle layer
+
+})
+dispatch.on("viewByCbsa", function(){
+//toggle layer
+
+})
+dispatch.on("changeIndustry", function(industry){
+//toggle layer
+console.log(industry)
+
+if(map.getZoom() == 3){
+map.setPaintProperty("county-fill", 'fill-color', [
+"interpolate",
+["linear"],
+["get", industry],
+1,
+"#cfe8f3",
+2,
+"#a2d4ec",
+3,
+"#73bfe2",
+4,
+"#46abdb",
+5,
+"#1696d2",
+6,
+"#12719e",
+7,
+"#0a4c6a",
+8,
+"#062635"
+]);
+map.setPaintProperty("county-fill", 'fill-outline-color', [
+"interpolate",
+["linear"],
+["get", industry],
+1,
+"#cfe8f3",
+2,
+"#a2d4ec",
+3,
+"#73bfe2",
+4,
+"#46abdb",
+5,
+"#1696d2",
+6,
+"#12719e",
+7,
+"#0a4c6a",
+8,
+"#062635"
+]);
+
+}else{
+
+map.setPaintProperty("tract-fill", 'fill-color', [
+"interpolate",
+["linear"],
+["get", industry],
+1,
+"#cfe8f3",
+2,
+"#a2d4ec",
+3,
+"#73bfe2",
+4,
+"#46abdb",
+5,
+"#1696d2",
+6,
+"#12719e",
+7,
+"#0a4c6a",
+8,
+"#062635"
+]);
+}
+})
+})
+}
+
 
 function resizeBarChart(){
 
 }
 
-function init(
-	rawCbsaAverageData,
-	rawCountyAverageData,
-	rawCountyBoundingBoxData,
-	rawCbsaBoundingBoxData,
-	rawCbsaToCounty,
-	rawCountyToCbsa
-){
-	cbsaAverageData = rawCbsaAverageData;
-	countyAverageData = rawCountyAverageData;
-	countyBoundingBoxData = rawCountyBoundingBoxData;
-	cbsaBoundingBoxData = rawCbsaBoundingBoxData;
-	cbsaToCounty = rawCbsaToCounty;
-	countyToCbsa = rawCountyToCbsa;
+function initControls(){
+d3.select("#zoomOutIcon")
+.on("click", zoomOut)
+}
 
-	initBarChart(countyAverageData);
-	initMap();
+function init(
+rawCbsaAverageData,
+rawCountyAverageData,
+rawCountyBoundingBoxData,
+rawCbsaBoundingBoxData,
+rawCbsaToCounty,
+rawCountyToCbsa
+){	
+usAverageData = rawCbsaAverageData;
+cbsaAverageData = rawCbsaAverageData;
+countyAverageData = rawCountyAverageData;
+countyBoundingBoxData = rawCountyBoundingBoxData;
+cbsaBoundingBoxData = rawCbsaBoundingBoxData;
+cbsaToCounty = rawCbsaToCounty;
+countyToCbsa = rawCountyToCbsa;
+
+initBarChart(countyAverageData);
+initMap();
+initControls();
 }
 
 
 d3.json("data/countyMedian.json").then(function(rawCbsaAverageData){
-	init(false, rawCbsaAverageData, false, false, false, false)
+init(false, rawCbsaAverageData, false, false, false, false)
 })
